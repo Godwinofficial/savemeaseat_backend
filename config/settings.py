e9ff2287ae9167eead479edd75d05d4ff1a730b8
Godwinofficial <godwinbanda19@gmail.com>
2025-07-02 12:42:46 +0200

    fixed sections 24

--- config/settings.py
+++ config/settings.py
@@ -31,8 +31,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
