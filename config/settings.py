3738c4284b2654a035157229d1f3a4ab0d02af58
Godwinofficial <godwinbanda19@gmail.com>
2025-12-16 18:52:28 +0200

    fixed gifts

--- config/settings.py
+++ config/settings.py
@@ -25,16 +25,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECRET_KEY = 'django-insecure-!+tihbtuq5%j3)2-fd9d5=sq31=*ayo^(kuh_a&y(v5)qq5y-8'
+SECRET_KEY = 'django-insecure-!+tihbtuq5%j3)2-fd9d5=sq31=*ayo^(kuh_a&y(v5)qq5y-8'
 
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+# DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-# DEBUG = True
+DEBUG = True
 
 # ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
@@ -104,15 +104,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 
-DATABASE_URL = os.environ.get('DATABASE_URL')
+# DATABASE_URL = os.environ.get('DATABASE_URL')
 
-# Fallback (optional): raise an error if it's not set
-if not DATABASE_URL:
-    raise Exception("DATABASE_URL environment variable not set")
+# # Fallback (optional): raise an error if it's not set
+# if not DATABASE_URL:
+#     raise Exception("DATABASE_URL environment variable not set")
 
-DATABASES = {
-    "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
-}
+# DATABASES = {
+#     "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+# }
 
 # ...........
 
@@ -122,21 +122,21 @@ DATABASES = {
 # BASE_DIR = Path(__file__).resolve().parent.parent
 
 # # Database configuration
-# DATABASE_URL = os.environ.get('DATABASE_URL')
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
-# if DATABASE_URL:
-#     print("✅ Using PostgreSQL database from DATABASE_URL")
-#     DATABASES = {
-#         "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
-#     }
-# else:
-#     print("⚠️ DATABASE_URL not set. Using SQLite database instead.")
-#     DATABASES = {
-#         "default": {
-#             "ENGINE": "django.db.backends.sqlite3",
-#             "NAME": BASE_DIR / 'db.sqlite3',
-#         }
-#     }
+if DATABASE_URL:
+    print("✅ Using PostgreSQL database from DATABASE_URL")
+    DATABASES = {
+        "default": dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    }
+else:
+    print("⚠️ DATABASE_URL not set. Using SQLite database instead.")
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # .......
